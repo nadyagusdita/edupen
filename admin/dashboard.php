64fe073512a7c89d61d3fdd33d53059a04b5d3da
nadyagusdita <72004685+nadyagusdita@users.noreply.github.com>
2022-07-04 10:51:22 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,32 +7,7 @@ $id = $_SESSION['id_user'];
 ?>
 
 <body>
-    <script>
-        < script type = "text/javascript"
-        src = "https://www.gstatic.com/charts/loader.js" >
-    </script>
-    <script type="text/javascript">
-        google.charts.load('current', {
-            'packages': ['corechart']
-        });
-        google.charts.setOnLoadCallback(drawChart);
 
-        function drawChart() {
-
-            var data = google.visualization.arrayToDataTable([
-                ['Nama_JenisTulisan', 'Bobot'],
-                <?php
-                while ($chart = mysqli_fetch_assoc($result)) {
-                    echo "['" . $chart['Nama_JenisTulisan'] . "'," . $chart['bobot'] . "],";
-                }
-                ?>
-            ]);
-
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-            chart.draw(data);
-        }
-    </script>
     <div id="app">
         <?php include 'sidebar.php' ?>
         <div id="main">
